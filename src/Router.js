3181cde9df10ddce136242df44c3c230e01a5a37
Komal Suthar <komalsuthar@whatsadeal.com>
2021-02-25 15:25:36 +0530

    First Commit

---
+++ src/Router.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Product from "./components/Product";
+import ProductItems from "./components/ProductItems";
+import About from "./components/About";
+import Nav from "./components/Nav";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import OrderSummary from "./components/OrderSummary";
+import PlaceOrder from "./components/PlaceOrder";
+
+
+function RouterFunction() {
+  return (
+    <div>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route exact path="/product" component={Product} />
+          <Route path="/placeorder" component={PlaceOrder} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/ordersummary" component={OrderSummary} />
+          <Route path="/product/:id" component={ProductItems} />
+          <Route path="*" component={() => <h2>404 Not Found </h2>} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default RouterFunction;
